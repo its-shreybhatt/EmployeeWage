9d82be98405c32733b98a38a81ded678be7da37f
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-23 14:30:38 +0530

    Modified : Using Switch Case

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -9,17 +9,24 @@ public class EmployeeWage {
         int parttime = 0;
         Random value1 = new Random();
         int attendance = value1.nextInt(2);
-        if (attendance == 0) {
-            System.out.println("Present");
-            Random value2 = new Random();
-            int time = value2.nextInt(2);
-            if (time == 0) {
-                System.out.println("Employee is Parttime");
-                System.out.println("Daily wage = " + 20 * 4);
-            } else {
-                System.out.println("Employee is Fulltime");
-                System.out.println("Daily wage = " + 20 * 8);
-            }
-        } else System.out.println("Absent .. No wage");
+        switch (attendance) {
+            case 1:
+                System.out.println("Absent .. No wage");
+                break;
+            case 0:
+                System.out.println("Present");
+                Random value2 = new Random();
+                int time = value2.nextInt(2);
+                switch (time) {
+                    case 0:
+                        System.out.println("Employee is Parttime");
+                        System.out.println("Daily wage = " + 20 * 4);
+                        break;
+                    case 1:
+                        System.out.println("Employee is Fulltime");
+                        System.out.println("Daily wage = " + 20 * 8);
+                        break;
+                }
+        }
     }
 }
