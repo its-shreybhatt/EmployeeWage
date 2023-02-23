f68aea10d0adac9fa6dc49bf40d36fee2d7ab3cd
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-23 14:18:14 +0530

    Added : Daily Wage of Employee for Parttime

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -5,11 +5,21 @@ public class EmployeeWage {
         System.out.println("welcome to employee wage computation program !");
         int present = 0;
         int absent = 0;
+        int fulltime = 0;
+        int parttime = 0;
         Random value1 = new Random();
         int attendance = value1.nextInt(2);
         if (attendance == 0) {
-            System.out.println("present");
-            System.out.println("Daily employee wage = " + 20 * 8 + " rupees");
-        } else System.out.println("absent .. No wage");
+            System.out.println("Present");
+            Random value2 = new Random();
+            int time = value2.nextInt(2);
+            if (time == 0) {
+                System.out.println("Employee is Parttime");
+                System.out.println("Daily wage = " + 20 * 4);
+            } else {
+                System.out.println("Employee is Fulltime");
+                System.out.println("Daily wage = " + 20 * 8);
+            }
+        } else System.out.println("Absent .. No wage");
     }
 }
