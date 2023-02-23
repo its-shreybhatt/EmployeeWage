bb3ca9f028cf6fabfd04819d416808becc0c9e10
its-shreybhatt <shreybhatt2@gmail.com>
2023-02-23 14:01:16 +0530

    Added : Random Attendance of Employee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program !");
+        int present = 0;
+        int absent = 0;
+        Random value1 = new Random();
+        int attendance = value1.nextInt(2);
+        if (attendance == 0) {
+            System.out.println("present");
+        }
+        else System.out.println("absent");
     }
 }
