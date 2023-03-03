1da83b4101b65a3202093dbba6e47c6df7eecc64
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-03 16:15:29 +0530

    Modified : Code by Adding Multiple Companies

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,25 +1,32 @@
-import java.util.Random;
-
 public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NO_OF_WORKING_DAYS = 20;
-    public static final int MAX_HOURS_IN_MONTH = 100;
-    static int totalEmpHrs = 0;
+
+    private final String Company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+
+    public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        this.Company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
 
     public static void main(String[] args) {
-
-        condition();
-        print();
+        EmployeeWage dMart = new EmployeeWage("Dmart", 20,20,100);
+        dMart.company();
+        EmployeeWage reliance = new EmployeeWage("Reliance", 25,25,150);
+        reliance.company();
 
     }
-    public static void condition() {
+    public void company() {
 //        Variables
-        int empHrs = 0, totalWorkingDays = 0;
+        int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
 //        Computation
-        while (totalEmpHrs <= MAX_HOURS_IN_MONTH &&
-                totalWorkingDays < NO_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= maxHoursPerMonth &&
+                totalWorkingDays < numOfWorkingDays) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -33,12 +40,9 @@ public class EmployeeWage {
                     empHrs = 0;
             }
             totalEmpHrs += empHrs;
-            System.out.println("Day: " + totalWorkingDays + " Emp Hrs: " + empHrs);
+//            System.out.println("Day: " + totalWorkingDays + " Emp Hrs: " + empHrs);
         }
-    }
-
-    public static void print() {
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage = " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp Wage for " + Company +" = " + totalEmpWage);
     }
 }
